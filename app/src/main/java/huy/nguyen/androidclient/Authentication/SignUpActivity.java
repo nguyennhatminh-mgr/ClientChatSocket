c299bf8c56d409fc34cd855318fc7f7134683fd8
nhatminh123 <nguyennhatminh12a1@gmail.com>
2019-10-24 09:56:00 +0700

    Commit sign up

--- app/src/main/java/huy/nguyen/androidclient/Authentication/SignUpActivity.java
+++ app/src/main/java/huy/nguyen/androidclient/Authentication/SignUpActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +15,10 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.regex.Pattern;
 
 import huy.nguyen.androidclient.AuthenUtil;
+import huy.nguyen.androidclient.Home.HomeActivity;
 import huy.nguyen.androidclient.R;
 import huy.nguyen.androidclient.SignupCallback;
 
@@ -27,7 +31,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        createSocket();
+//        createSocket();
         addControls();
         txtBackToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,16 +45,19 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(SignUpActivity.this,"abc",Toast.LENGTH_SHORT).show();
-                AuthenUtil.doSignUp("a", "b", "c", new SignupCallback() {
-                    @Override
-                    public void notify(String result) {
-                        Toast.makeText(SignUpActivity.this,result,Toast.LENGTH_SHORT).show();
-                        Log.e("ok",result);
-                    }
-                });
+//                AuthenUtil.doSignUp("a", "b", "c", new SignupCallback() {
+//                    @Override
+//                    public void notify(String result) {
+//                        Toast.makeText(SignUpActivity.this,result,Toast.LENGTH_SHORT).show();
+//                        Log.e("ok",result);
+//                    }
+//                });
+                Intent intent=new Intent(SignUpActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
-
+        checkRegression();
     }
 
     private void createSocket() {
@@ -68,6 +75,51 @@ public class SignUpActivity extends AppCompatActivity {
         }).start();
     }
 
+    private void checkRegression(){
+        edtUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(edtUsername.getText().toString().matches("[0-9a-zA-Z_]*")){
+
+                }
+                else{
+//                    Toast.makeText(SignUpActivity.this,"Not valid password",Toast.LENGTH_SHORT).show();
+                    edtUsername.setError("Not valid password");
+                }
+            }
+        });
+        edtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(edtPassword.getText().toString().matches("([0-9][0-9][0-9][0-9][0-9][0-9])*")){
+
+                }
+                else{
+                    edtPassword.setError("Password is at least 6 characters");
+                }
+            }
+        });
+    }
     private void addControls() {
         edtAccountName=findViewById(R.id.edtAccountNameSignUp);
         edtUsername=findViewById(R.id.edtUsernameSignUp);
