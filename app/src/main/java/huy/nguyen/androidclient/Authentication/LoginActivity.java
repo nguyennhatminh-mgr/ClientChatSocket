893a2b10ed49fda03d31dec1eb8391f18c29ad67
nhatminh123 <nguyennhatminh12a1@gmail.com>
2019-10-29 09:16:06 +0700

    minh commit to merge

--- app/src/main/java/huy/nguyen/androidclient/Authentication/LoginActivity.java
+++ app/src/main/java/huy/nguyen/androidclient/Authentication/LoginActivity.java
@@ -170,7 +170,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Socket socket = new Socket(TVTT_IP, 8080);
+                    Socket socket = new Socket("192.168.43.62", 8080);
                     SocketUtil.setSocket(socket);
                 } catch (IOException e) {
                     e.printStackTrace();
--- app/src/main/java/huy/nguyen/androidclient/Home/HomeActivity.java
+++ app/src/main/java/huy/nguyen/androidclient/Home/HomeActivity.java
@@ -22,6 +22,8 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 import huy.nguyen.androidclient.MainActivity;
+import huy.nguyen.androidclient.Message.MessageGroupActivity;
+import huy.nguyen.androidclient.Model.User;
 import huy.nguyen.androidclient.Model.UserInfo;
 import huy.nguyen.androidclient.R;
 import huy.nguyen.androidclient.Utilities.Interface.OnlineUserCallback;
@@ -36,6 +38,9 @@ import static huy.nguyen.androidclient.Utilities.SocketUtil.socketMap;
 public class HomeActivity extends AppCompatActivity {
 
     public static ArrayList<UserInfo> userArrayList;
+    ArrayList<UserInfo> listUserInGroup;
+    HomeUserAdpter groupUserAdapter;
+    ListView listViewGroup;
     HomeUserAdpter homeUserAdpter;
     ListView listView;
     Button button;
@@ -58,12 +63,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         listView = findViewById(R.id.lvListUserInHome);
+        listViewGroup=findViewById(R.id.lvUserInGroup);
         button = findViewById(R.id.button);
 //        checkBox=findViewById(R.id.chkAddToGroup);
         btnCreateGroup=findViewById(R.id.btnCreateGroup);
         userArrayList = new ArrayList<>();
         homeUserAdpter = new HomeUserAdpter(HomeActivity.this, userArrayList);
         listView.setAdapter(homeUserAdpter);
+
+        listUserInGroup=new ArrayList<>();
+        groupUserAdapter=new HomeUserAdpter(HomeActivity.this,listUserInGroup);
+        listViewGroup.setAdapter(groupUserAdapter);
 
 
         SocketUtil.retriveOnlineUser(new OnlineUserCallback() {
@@ -74,7 +84,7 @@ public class HomeActivity extends AppCompatActivity {
                     public void run() {
                         userArrayList.clear();
                         userArrayList.addAll(onlineList);
-                        Toast.makeText(HomeActivity.this, userArrayList.toString(), Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(HomeActivity.this, userArrayList.toString(), Toast.LENGTH_SHORT).show();
                         homeUserAdpter.notifyDataSetChanged();
                     }
                 });
@@ -106,13 +116,77 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         });
+
         initMyServerSocket();
+        initGroupSocket();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doLogOut();
             }
         });
+
+        btnCreateGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Socket socket=SocketUtil.getSocket();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            PrintWriter output=new PrintWriter(socket.getOutputStream());
+                            output.write("JOIN_TO_GROUP"+"\n");
+                            output.write(SocketUtil.getMyIp()+"\n");
+                            output.flush();
+                            Log.e("123","btnCreateCLick");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+                Intent intent=new Intent(HomeActivity.this, MessageGroupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initGroupSocket() {
+        final Socket socket=SocketUtil.getSocket();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    BufferedReader input=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    while (true){
+                        String msg=input.readLine();
+                        if(msg.equals("NOTIFY_JOIN_TO_GROUP")){
+                            Log.e("123","NOTIFY_JOIN_TO_GROUP");
+                            String temp;
+//                            Log.e("123 temp", temp );
+                            while (!(temp=input.readLine()).equals("END_NOTIFY_JOIN_TO_GROUP")){
+                                if(temp!=null){
+                                    String[] temp1=temp.split("[:]");
+                                    if(temp1.length==2){
+                                        final UserInfo user=new UserInfo(temp1[0],temp1[1]);
+//                            Log.e("123",temp1.toString());
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                listUserInGroup.add(user);
+                                                groupUserAdapter.notifyDataSetChanged();
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private void initMyServerSocket() {
@@ -177,6 +251,7 @@ public class HomeActivity extends AppCompatActivity {
                         }
                         break;
                     }
+
                 }
 
             } catch (Exception e) {
